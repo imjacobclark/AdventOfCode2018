297a3b12cb516eec93e40be8920d24152f662735
Jacob Clark <jacob.clark@bbc.co.uk>
2018-12-03 21:26:23 +0000

    Test name

--- src/test/java/xyz/jacobclark/day2/BoxesTest.java
+++ src/test/java/xyz/jacobclark/day2/BoxesTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
-public class ChecksumCalculatorTest {
+public class BoxesTest {
     @Test
     public void shouldReturn0WhenNoInputIsProvided() throws Exception {
         List<Box> boxes = new ArrayList<>();
